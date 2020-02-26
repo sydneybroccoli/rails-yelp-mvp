c3ddcd06884c85d3036d8865fbd46b9c0c358d0a
Sydney Barovsky <sbarovsky@gmail.com>
2020-02-26 16:29:53 +0800

    working but basic yelp page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  def index
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(restaurant_id: @restaurant.id)
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,24 @@ puts 'Cleaning database...'
 Restaurant.destroy_all
 
 puts 'Creating restaurants...'
-5.times do
+10.times do
   restaurant = Restaurant.new(
     name: Faker::Restaurant.name,
     address: Faker::Address.street_address,
     phone_number: Faker::PhoneNumber.phone_number_with_country_code,
     category: Restaurant::CATEGORY.sample
   )
+  restaurant.save
+
+  5.times do
+    review = Review.new(
+      rating: (0..5).to_a.sample,
+      content: Faker::Hipster.sentences.join(' '),
+      restaurant_id: restaurant.id
+    )
+    review.save!
+    # restaurant.reviews = review
+  end
+
   restaurant.save!
 end
